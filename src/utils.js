295ce57c00379af5bc842876593321775411a290
LegendaryB <6391022+LegendaryB@users.noreply.github.com>
2022-06-09 00:22:14 +0200

    Added new utility function.

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,7 @@
+export const between = (x, min, max) => {
+    return x >= min && x <= max;
+}
+
 export const firstLetterToUpperCase = (value) => {
     return value.charAt(0).toUpperCase() + value.slice(1)
 }
