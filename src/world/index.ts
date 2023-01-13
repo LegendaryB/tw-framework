a3e25627f2674db4ef59fcdd53c950d416288bea
LegendaryB <6391022+LegendaryB@users.noreply.github.com>
2023-01-13 15:17:28 +0100

    Moved XML conversion into separate file.

--- src/world/index.ts
+++ src/world/index.ts
@@ -2,6 +2,8 @@ import { WorldConfiguration } from './WorldConfiguration';
 import { BuildingInfoKeys, BuildingInfo } from './BuildingInfo';
 import { UnitInfoKeys, UnitInfo } from './UnitInfo';
 
+import { xmlToJson } from './xmlToJson';
+
 const WORLD_CONFIG_ENDPOINT = 'interface.php?func=get_config';
 const UNIT_INFO_ENDPOINT = 'interface.php?func=get_unit_info';
 const BUILDING_INFO_ENDPOINT = 'interface.php?func=get_building_info';
@@ -18,59 +20,6 @@ const request = async (endpoint: string): Promise<string> => {
     let body = await response.text();
 
     return body;
-}
-
-const letterToUpperCaseAt = (input: string, index: number) => {
-    return input.substring(0, index) + input.charAt(index).toUpperCase() + input.substr(index + 1);
-}
-
-const getIndexesOf = (input: string, searchElement: string) => {
-    let results = [], i = -1;
-
-    while ((i = input.indexOf(searchElement, i + 1)) >= 0) {
-        results.push(i);
-    }
-
-    return results;
-}
-
-const createKeyForProperty = (propertyKey: string) => {
-    let indexes = getIndexesOf(propertyKey, '_');
-    propertyKey = letterToUpperCaseAt(propertyKey, 0);
-
-    for (const index of indexes) {
-        propertyKey = letterToUpperCaseAt(propertyKey, index + 1);
-    }
-
-    propertyKey = propertyKey.replaceAll('_', '');
-
-    return propertyKey;
-}
-
-const xmlToJson = (data: string) => {
-    let obj = {};
-
-    let parser = new DOMParser();
-    let xml = parser.parseFromString(data, 'text/xml');
-    let rootNode = xml.querySelector('config');
-
-    for (const node of rootNode.children) {
-        let key = createKeyForProperty(letterToUpperCaseAt(node.localName, 0));
-        obj[key] = {};
-
-        if (node.children.length == 0) {
-            obj[key] = Number(node.innerHTML);
-            continue;
-        }
-
-        for (const propNode of node.children) {
-            let propertyKey = createKeyForProperty(propNode.localName);
-            
-            obj[key][propertyKey] = Number(propNode.innerHTML);
-        }
-    }
-
-    return obj;
 }
 
 const get = async (endpoint: string, cacheKey: string): Promise<any> => {
