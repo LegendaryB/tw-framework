df2735ff4c039a6e4558876ae7e888f5250c7bcc
LegendaryB <6391022+LegendaryB@users.noreply.github.com>
2023-01-13 17:09:27 +0100

    Small fix

--- src/math/UnitRuntime.ts
+++ src/math/UnitRuntime.ts
@@ -2,16 +2,18 @@ import { UnitKeys } from '../common';
 import { calculateDistance } from './Coordinates';
 import { getUnitInfo } from '../world';
 
-const calculateUnitRuntime = async (startX: number, startY: number, targetX: number, targetY: number, unit: UnitKeys): Promise<number> => {
+const COORDINATE_SEPARATOR = '|';
+
+export const calculateUnitRuntime = async (startX: number, startY: number, targetX: number, targetY: number, unit: UnitKeys): Promise<number> => {
     let distance = calculateDistance(startX, startY, targetX, targetY);
     let unitSpeed = (await getUnitInfo())[unit].Speed;
 
     return distance * unitSpeed;
 }
 
-const calculateUnitRuntimeByCoordinates = (startCoordinates: string, targetCoordinates: string, unit: UnitKeys): Promise<number> => {
-    let [startX, startY] = startCoordinates.split('|').flatMap(coordinate => Number(coordinate));
-    let [targetX, targetY] = targetCoordinates.split('|').flatMap(coordinate => Number(coordinate));
+export const calculateUnitRuntimeByCoordinates = (startCoordinates: string, targetCoordinates: string, unit: UnitKeys): Promise<number> => {
+    let [startX, startY] = startCoordinates.split(COORDINATE_SEPARATOR).flatMap(coordinate => Number(coordinate));
+    let [targetX, targetY] = targetCoordinates.split(COORDINATE_SEPARATOR).flatMap(coordinate => Number(coordinate));
 
     return calculateUnitRuntime(
         startX,
